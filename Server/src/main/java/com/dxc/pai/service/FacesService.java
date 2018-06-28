8d5a1647533c62b38a36ede8e806b84c5e07bb7f
CallYana <18883947737@163.com>
2018-06-28 05:58:18 +0000

    Signed-off-by: CallYana <18883947737@163.com>

--- Server/src/main/java/com/dxc/pai/service/FacesService.java
+++ Server/src/main/java/com/dxc/pai/service/FacesService.java
@@ -29,27 +29,27 @@ public class FacesService {
 		JSONArray sexData = new JSONArray();
 		List<AllFaces> persons = allface.getPersons();
 		int count = 0;
-		int male = 0, famale = 0;
+		int male = 0, female = 0;
 		for(int i=0;i<persons.size();i++)
 		{
 			AllFaces person = persons.get(i);
 			count++;
-			if(person.getGender().equals("male"))
+			if(person.getGender().equals("Male"))
 			{
 				male++;
 			}
 			else
-				famale++;			
+				female++;			
 		}
 		JSONObject maleObj = new JSONObject();
 		maleObj.put("gender", "male");
 		maleObj.put("number", male);
 		JSONObject famaleObj = new JSONObject();
-		famaleObj.put("gender", "famale");
-		famaleObj.put("number", famale);
+		famaleObj.put("gender", "female");
+		famaleObj.put("number", female);
 		sexData.add(maleObj);
 		sexData.add(famaleObj);
-		System.out.println(sexData);
+		//System.out.println(sexData);
 		return sexData;
 		
 	}
@@ -70,15 +70,15 @@ public class FacesService {
 			int age = person.getAge();			
 			if(ages.containsKey(age))
 			{
-				System.out.println("have this age");
+				//System.out.println("have this age");
 				count  = (int) ages.get(age) +1 ;					
 			}			
 						
 			ages.put(age, count);
-			System.out.println(ages);
+			//System.out.println(ages);
 		}	
 		ageData = getJSONArrayByMap(ages);
-		System.out.println(ageData);
+		//System.out.println(ageData);
 		return ageData;
 		
 	}
@@ -90,7 +90,7 @@ public class FacesService {
 		for(Map.Entry<Integer, Integer> entry : ages.entrySet()) { 
 			JSONObject temp = new JSONObject();
 			temp.put("age",entry.getKey());
-			System.out.println(entry.getKey());
+			//System.out.println(entry.getKey());
 			temp.put("number", entry.getValue());
 			result.add(temp);
 		}
